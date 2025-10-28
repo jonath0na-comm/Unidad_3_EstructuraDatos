4a346474f94ebc5ad8a8853bcfa8345123a0d741
jonath0na-comm <jvargasarciniega@gmail.com>
2025-10-28 12:42:12 -0600

    act
    nuevo

--- src/Ejercicio1/datos/KardexDatos.java
+++ src/Ejercicio1/datos/KardexDatos.java
@@ -4,36 +4,47 @@
  */
 package Ejercicio1.datos;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 /**
  *
  * @author jonav
  */
 public class KardexDatos {
-    public static String [][] datos=
-            new String [10][3];
-    public static int index=0; // para saber cuantos llevo
-    
-public static double calcularPromedio() {
-    if (KardexDatos.index == 0) return 0;
+   
+    // Arreglo de datos
+    public static String[][] datos = new String[10][3];
 
-    double suma = 0;
-    for (int i = 0; i < KardexDatos.index; i++) {
-        suma += Double.parseDouble(KardexDatos.datos[i][2]);
-    }
-    return suma / KardexDatos.index;
-    // metodo que calcula el promedio segun lo vallan ingresando
-}
-public static int  buscar(String dato){
-    int a=0;
-    for (int i=0;i<datos.length;i++){
-        if (dato.equals(datos[i][0])){
-             a=i;
+    // Lista de Materias
+    public static List<Materias> Materias = new ArrayList<>();
+
+    // Para saber cuántos registros llevamos
+    public static int index = 0;
+
+    // Método que calcula el promedio según las calificaciones ingresadas
+    public static double calcularPromedio() {
+        if (index == 0) return 0;
+
+        double suma = 0;
+        for (int i = 0; i < index; i++) {
+            suma += Double.parseDouble(datos[i][2]);
         }
-        else {
-            a=-1;           
-        }
-     
+        return suma / index;
     }
-    return a;
-} 
+
+    // Método para buscar un dato en la primera columna del arreglo
+    public static int buscar(String dato) {
+        for (int i = 0; i < index; i++) {
+            if (dato.equals(datos[i][0])) {
+                return i; // retorna la posición si lo encuentra
+            }
+        }
+        return -1; // retorna -1 si no lo encuentra
+    }
+
+    // Método para agregar una Materia a la lista
+    public static void agregarMateria(Materias m) {
+        materias.add(m);
+    }
 }
---
+++ src/Ejercicio1/datos/Materias.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicio1.datos;
+
+/**
+ *
+ * @author Jonat
+ */
+public class Materias {
+    String nombre;
+    int calificaciòn;
+    int semestre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCalificaciòn() {
+        return calificaciòn;
+    }
+
+    public void setCalificaciòn(int calificaciòn) {
+        this.calificaciòn = calificaciòn;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+}
